eafccd07f4fad35be0e9609196933ead164a541c
yhyasyrian <yhya.syrian@gmail.com>
2022-01-19 13:20:23 +0200

    add progressive

--- main.py
+++ main.py
@@ -1,5 +1,22 @@
 import re
-from pytube import YouTube
+from pytube import CaptionQuery, Playlist, Stream, YouTube 
+import os, sys, shutil
+# Start Function For progressive
+def display_progress_bar(bytes_received: int, filesize: int, ch: str = "█", scale: float = 0.55) -> None:
+  columns = shutil.get_terminal_size().columns
+  max_width = int(columns * scale)
+  filled = int(round(max_width * bytes_received / float(filesize)))
+  remaining = max_width - filled
+  progress_bar = ch * filled + " " * remaining
+  percent = round(100.0 * bytes_received / float(filesize), 1)
+  text = f" ↳ |{progress_bar}| {percent}%\r"
+  sys.stdout.write(text)
+  sys.stdout.flush()
+def on_progress(stream: Stream, chunk: bytes, bytes_remaining: int) -> None:  # pylint: disable=W0613
+    filesize = stream.filesize
+    bytes_received = filesize - bytes_remaining
+    display_progress_bar(bytes_received, filesize)
+
 # https://youtu.be/p_bGaIKej_Q
 url = input("Enter Your Url YouTube For Download:")
 type = input("Enter Type File For Download video/audio:")
@@ -7,7 +24,7 @@ Quality = ['144p','240p','360p','480p','720p','1080p']
 QualityVideo = []
 PATH = "/data/data/com.termux/files/home/storage/shared/video"
 def end():
-  print("Finsh Download Video")
+  print("\nFinsh Download Video")
 if(type.lower() == "video"):
   video = YouTube(url)
   for result in video.streams.filter(progressive=True):
@@ -17,19 +34,9 @@ if(type.lower() == "video"):
         QualityVideo.append(i)
         break
   p = input("Enter Your The Quality "+  '|'.join(QualityVideo) +":")
-  if p == '144p':
-    size = 1.9
-  elif p == '240p':
-    size = 2.7
-  elif p == "360p":
-    size = 4.4
-  elif p == "480p":
-    size = 7.7
-  elif p == "720p":
-    size = 14.5
-  elif p == "1080p":
-    size = 27.61
-  sizeVideo = (video.length / 60) * size
-  print("Download Video Size Is : "+ str(sizeVideo))
+  size = video.streams.filter(progressive=True,res=p).order_by('resolution').desc().first().filesize / 1000000
+  print("Download Video Size Is : "+ str(round(size,1)) +"MG")
+  video.register_on_progress_callback(on_progress)
   video.streams.filter(progressive=True,res=p).order_by('resolution').desc().first().download(output_path=PATH)
   video.register_on_complete_callback(end())
+#end 
